74c6f8c070e8550b5d2e4ce9a8abefa67b7a8a1e
2021066717 <88358570+kimtaehwan11@users.noreply.github.com>
2022-09-23 13:36:47 +0900

    cpp
    int x; x = y - x; problem.
    is x == 0 when it is not defined? but the code says "no it isn't."

---
+++ baekjoon/undefined int problem.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	int n, x, y;
+	cin >> n;
+	for(int i=0; i<1; i++){
+		cin >> x >> y;
+		int x;
+		x = y - x;
+		cout << x << endl;
+	}
+	return 0;
+}
